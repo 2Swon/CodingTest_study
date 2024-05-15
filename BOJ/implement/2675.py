f880d056136d08a23d1b6ae88384419a4a1f31d0
2Swon <tjddnjs10617@naver.com>
2024-05-16 02:31:14 +0900

    cd study

--- BOJ/implement/2675.py
+++ BOJ/implement/2675.py
@@ -1,18 +1,11 @@
-# T = int(input())
-#
-# for _ in range(T):
-#     n, word = map(str, input().split())
-#     N = int(n)
-#
-#     for i in word:
-#         for j in range(N):
-#             print(i, end="")
-#
-#     print()
+T = int(input())
 
-print("         ,r'\"7")
-print("r`-_   ,'  ,/")
-print(" \. \". L_r'")
-print("   `~\/")
-print("      |")
-print("      |")
+for _ in range(T):
+    n, word = map(str, input().split())
+    N = int(n)
+
+    for i in word:
+        for j in range(N):
+            print(i, end="")
+
+    print()
